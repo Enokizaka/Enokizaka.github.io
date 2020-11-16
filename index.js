f903b3a4a42bd61e92c75ee27b4c15ead461020a
Kengo Enokizaka <atarashii0206@gmail.com>
2020-11-16 19:01:40 +0900

    pwa install-button v3

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const buttonInstall = document.querySelector(".install-button")
+buttonInstall.style.display = 'none'
 let deferredPrompt
 
 window.addEventListener("beforeinstallprompt", (e) => {
@@ -7,5 +8,21 @@ window.addEventListener("beforeinstallprompt", (e) => {
   // Stash the event so it can be triggered later.
   deferredPrompt = e
   // Update UI notify the user they can install the PWA
+  buttonInstall.style.display = 'block'
   showInstallPromotion()
+})
+
+buttonInstall.addEventListener("click", (e) => {
+  // Hide the app provided install promotion
+  hideMyInstallPromotion()
+  // Show the install prompt
+  deferredPrompt.prompt()
+  // Wait for the user to respond to the prompt
+  deferredPrompt.userChoice.then((choiceResult) => {
+    if (choiceResult.outcome === "accepted") {
+      console.log("User accepted the install prompt")
+    } else {
+      console.log("User dismissed the install prompt")
+    }
+  })
 })
