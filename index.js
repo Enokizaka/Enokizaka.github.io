d4e2a527cb3a0d7e702f722c314f6ab2757dbe89
Kengo Enokizaka <atarashii0206@gmail.com>
2020-11-16 19:40:49 +0900

    pwa install-button v10

--- index.js
+++ index.js
@@ -1,36 +1,34 @@
-let deferredPrompt
-let buttonInstall
-
-window.addEventListener("beforeinstallprompt", (e) => {
-  console.log("before install prompt")
-  buttonInstall.style.display = "block"
-  // Prevent the mini-infobar from appearing on mobile
-  e.preventDefault()
-  // Stash the event so it can be triggered later.
-  deferredPrompt = e
-  // Update UI notify the user they can install the PWA
-  //   buttonInstall.style.display = "block"
-  //   showInstallPromotion()
-})
-
 window.addEventListener("DOMContentLoaded", () => {
   console.log("dom content loaded")
-  buttonInstall = document.querySelector(".install-button")
+  let deferredPrompt
+  let buttonInstall = document.querySelector(".install-button")
   buttonInstall.style.display = "none"
+
+  window.addEventListener("beforeinstallprompt", (e) => {
+    console.log("before install prompt")
+    buttonInstall.style.display = "block"
+    // Prevent the mini-infobar from appearing on mobile
+    e.preventDefault()
+    // Stash the event so it can be triggered later.
+    deferredPrompt = e
+    // Update UI notify the user they can install the PWA
+    //   buttonInstall.style.display = "block"
+    //   showInstallPromotion()
+  })
 
   buttonInstall.addEventListener("click", (e) => {
     // Hide the app provided install promotion
     // hideMyInstallPromotion()
     // Show the install prompt
     deferredPrompt.prompt()
-    // Wait for the user to respond to the prompt
-    deferredPrompt.userChoice.then((choiceResult) => {
-      if (choiceResult.outcome === "accepted") {
-        console.log("User accepted the install prompt")
-      } else {
-        console.log("User dismissed the install prompt")
-      }
-    })
+    // // Wait for the user to respond to the prompt
+    // deferredPrompt.userChoice.then((choiceResult) => {
+    //   if (choiceResult.outcome === "accepted") {
+    //     console.log("User accepted the install prompt")
+    //   } else {
+    //     console.log("User dismissed the install prompt")
+    //   }
+    // })
   })
 })
 
@@ -39,20 +37,20 @@ window.addEventListener("appinstalled", (evt) => {
   console.log("INSTALL: Success")
 })
 
-async function isInstalled() {
-  // For iOS
-  if (window.navigator.standalone) return true
+// async function isInstalled() {
+//   // For iOS
+//   if (window.navigator.standalone) return true
 
-  // For Android
-  if (window.matchMedia("(display-mode: standalone)").matches) return true
+//   // For Android
+//   if (window.matchMedia("(display-mode: standalone)").matches) return true
 
-  const relatedApps = await navigator.getInstalledRelatedApps()
-  relatedApps.forEach((app) => {
-    console.log(app.id, app.platform, app.url)
-  })
+//   const relatedApps = await navigator.getInstalledRelatedApps()
+//   relatedApps.forEach((app) => {
+//     console.log(app.id, app.platform, app.url)
+//   })
 
-  // If neither is true, it's not installed
-  return false
-}
+//   // If neither is true, it's not installed
+//   return false
+// }
 
-isInstalled()
+// isInstalled()
