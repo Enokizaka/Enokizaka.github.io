744e38974ee0e8147147ebb52a03688c23b86e23
Kengo Enokizaka <atarashii0206@gmail.com>
2020-11-16 19:04:08 +0900

    pwa install-button v4

--- index.js
+++ index.js
@@ -1,28 +1,30 @@
-const buttonInstall = document.querySelector(".install-button")
-buttonInstall.style.display = 'none'
-let deferredPrompt
+window.addEventListener("DOMContentLoaded", () => {
+  const buttonInstall = document.querySelector(".install-button")
+  buttonInstall.style.display = "none"
+  let deferredPrompt
 
-window.addEventListener("beforeinstallprompt", (e) => {
-  // Prevent the mini-infobar from appearing on mobile
-  e.preventDefault()
-  // Stash the event so it can be triggered later.
-  deferredPrompt = e
-  // Update UI notify the user they can install the PWA
-  buttonInstall.style.display = 'block'
-  showInstallPromotion()
-})
+  window.addEventListener("beforeinstallprompt", (e) => {
+    // Prevent the mini-infobar from appearing on mobile
+    e.preventDefault()
+    // Stash the event so it can be triggered later.
+    deferredPrompt = e
+    // Update UI notify the user they can install the PWA
+    buttonInstall.style.display = "block"
+    showInstallPromotion()
+  })
 
-buttonInstall.addEventListener("click", (e) => {
-  // Hide the app provided install promotion
-  hideMyInstallPromotion()
-  // Show the install prompt
-  deferredPrompt.prompt()
-  // Wait for the user to respond to the prompt
-  deferredPrompt.userChoice.then((choiceResult) => {
-    if (choiceResult.outcome === "accepted") {
-      console.log("User accepted the install prompt")
-    } else {
-      console.log("User dismissed the install prompt")
-    }
+  buttonInstall.addEventListener("click", (e) => {
+    // Hide the app provided install promotion
+    hideMyInstallPromotion()
+    // Show the install prompt
+    deferredPrompt.prompt()
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === "accepted") {
+        console.log("User accepted the install prompt")
+      } else {
+        console.log("User dismissed the install prompt")
+      }
+    })
   })
 })
